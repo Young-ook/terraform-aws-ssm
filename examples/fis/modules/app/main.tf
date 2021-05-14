c5518840481d72269efcc18fd7b155029510993b
Young-ook Kim <youngook.kim@gmail.com>
2021-05-15 08:47:00 +0900

    fix(fis): allow icmpv4 traffic between ec2 instances

--- examples/fis/modules/app/main.tf
+++ examples/fis/modules/app/main.tf
@@ -58,6 +58,13 @@ resource "aws_security_group" "alb_aware" {
     security_groups = [aws_security_group.alb.id]
   }
 
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [var.cidr]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
