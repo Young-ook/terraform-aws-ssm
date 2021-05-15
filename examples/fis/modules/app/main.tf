66d076147c5c8e24d7a8101853c32dd4ae9a9594
Young-ook <youngook.kim@gmail.com>
2021-05-15 09:03:50 +0900

    fix(fis): allow icmpv4 traffic between ec2 instances (#14)
    * fix(fis): allow icmpv4 traffic between ec2 instances
    
    * fix(fis): correct sg mapping for ec2

--- examples/fis/modules/app/main.tf
+++ examples/fis/modules/app/main.tf
@@ -58,6 +58,13 @@ resource "aws_security_group" "alb_aware" {
     security_groups = [aws_security_group.alb.id]
   }
 
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [var.cidr]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -109,7 +116,7 @@ module "ec2" {
       max_size          = 3
       desired_size      = 3
       instance_type     = "t3.small"
-      security_groups   = [aws_security_group.alb.id]
+      security_groups   = [aws_security_group.alb_aware.id]
       target_group_arns = [aws_lb_target_group.http.arn]
       user_data         = "#!/bin/bash\namazon-linux-extras install nginx1\nsystemctl start nginx"
     }
