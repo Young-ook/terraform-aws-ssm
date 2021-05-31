e08575f985610944189c9dd2f2b4c44bf4ea0f71
Young-ook Kim <youngook.kim@gmail.com>
2021-05-31 12:33:42 +0900

    fix(fis): silent delete script

--- examples/fis/aws-fis.tf
+++ examples/fis/aws-fis.tf
@@ -96,7 +96,7 @@ resource "local_file" "delete-templates" {
     "#!/bin/bash -ex",
     "OUTPUT='.fis_cli_result'",
     "while read id; do",
-    "  aws fis delete-experiment-template --id $${id} --output text --query 'experimentTemplate.id'",
+    "  aws fis delete-experiment-template --id $${id} --output text --query 'experimentTemplate.id' 2>&1 > /dev/null",
     "done < $${OUTPUT}",
     "rm $${OUTPUT}",
     ]
