14e5cfedcf38723e40a5df5e7de8dd3426bf163b
Young-ook Kim <youngook.kim@gmail.com>
2021-08-02 16:50:39 +0900

    feat(ssm): autoscaling warm-pools

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,25 @@
 # output variables 
 
-output "asg" {
-  description = "The EC2 autoscaling group attributes"
-  value       = aws_autoscaling_group.asg
+output "cluster" {
+  description = "The EC2 autoscaling groups attributes"
+  value = {
+    name = local.name
+    data_plane = {
+      node_groups = local.node_groups_enabled ? aws_autoscaling_group.ng : null
+      warm_pools  = local.warm_pools_enabled ? aws_autoscaling_group.wp : null
+    }
+  }
 }
 
 output "role" {
-  description = "The attribute of IAM role for EC2 autoscaling group"
+  description = "The attribute of IAM role for EC2 autoscaling groups"
   value       = aws_iam_role.asg
+}
+
+output "features" {
+  description = "Features configurations for the EC2 autoscaling groups"
+  value = {
+    "node_groups_enabled" = local.node_groups_enabled
+    "warm_pools_enabled"  = local.warm_pools_enabled
+  }
 }
