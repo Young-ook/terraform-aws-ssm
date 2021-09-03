84ec3b612a4ea6774761d5689f6e567b9ecc4871
Young-ook <youngook.kim@gmail.com>
2021-09-03 23:58:00 +0900

    fix(ssm-doc): ssm document association (#59)

--- examples/ssm-document/main.tf
+++ examples/ssm-document/main.tf
@@ -31,3 +31,34 @@ resource "aws_ssm_document" "cwagent" {
   document_type   = "Command"
   content         = file("${path.module}/templates/cwagent.yaml")
 }
+
+resource "aws_ssm_association" "cwagent" {
+  depends_on = [module.ec2]
+  name       = aws_ssm_document.cwagent.name
+
+  targets {
+    key    = "tag:env"
+    values = ["dev"]
+  }
+}
+
+resource "time_sleep" "wait" {
+  depends_on      = [aws_ssm_association.cwagent]
+  create_duration = "30s"
+}
+
+resource "aws_ssm_association" "diskfull" {
+  depends_on = [time_sleep.wait]
+  name       = aws_ssm_document.diskfull.name
+
+  targets {
+    key    = "tag:env"
+    values = ["dev"]
+  }
+
+  parameters = {
+    DurationSeconds = "60"
+    Workers         = "4"
+    Percent         = "70"
+  }
+}
