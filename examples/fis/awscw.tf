8846720b2f32db922a1757968f6622a0f5502af6
Young-ook Kim <youngook.kim@gmail.com>
2022-04-18 18:01:21 +0900

    chore(fis): grooming awscw resources

--- examples/fis/awscw.tf
+++ examples/fis/awscw.tf
@@ -31,3 +31,54 @@ resource "aws_ssm_association" "start-cwagent" {
     action = "start"
   }
 }
+
+### application/monitoring
+resource "aws_cloudwatch_metric_alarm" "cpu" {
+  alarm_name                = local.cw_cpu_alarm_name
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  tags                      = merge(local.default-tags, var.tags)
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 3
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 60
+  statistic                 = "Average"
+  threshold                 = 60
+  insufficient_data_actions = []
+
+  dimensions = {
+    AutoScalingGroupName = module.ec2.cluster.data_plane.node_groups.baseline.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "api-p90" {
+  alarm_name          = local.cw_api_p90_alarm_name
+  alarm_description   = "This metric monitors percentile of response latency"
+  tags                = merge(local.default-tags, var.tags)
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "TargetResponseTime"
+  namespace           = "AWS/ApplicationELB"
+  period              = 60
+  unit                = "Seconds"
+  threshold           = 0.1
+  extended_statistic  = "p90"
+
+  dimensions = {
+    LoadBalancer = aws_lb.alb.arn_suffix
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "api-avg" {
+  alarm_name          = local.cw_api_avg_alarm_name
+  alarm_description   = "This metric monitors average time of response latency"
+  tags                = merge(local.default-tags, var.tags)
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "TargetResponseTime"
+  namespace           = "AWS/ApplicationELB"
+  period              = 60
+  unit                = "Seconds"
+  statistic           = "Average"
+  threshold           = 0.1
+
