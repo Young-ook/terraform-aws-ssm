3749890ebe35c63f0ef47429e58dfd09d887cedc
Young-ook Kim <youngook.kim@gmail.com>
2022-12-24 20:37:07 +0900

    chore(eip): migration eip auto-association example

--- examples/eip/default.auto.tfvars
+++
@@ -1,6 +0,0 @@
-aws_region = "ap-northeast-2"
-name       = "ssm-eip"
-tags = {
-  env    = "dev"
-  region = "seoul"
-}
--- examples/eip/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ec2" {
-  description = "The generated AWS EC2 autoscaling group"
-  value       = module.ec2.cluster
-}
